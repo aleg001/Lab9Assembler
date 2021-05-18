9feb9866e8fb471d47a752efbed8418d4f98ba65
Ale Gómez <60187741+aleg001@users.noreply.github.com>
2021-05-17 18:04:54 -0600

    Se crean los archivos y define el menú

---
+++ Lab9.py
@@ -0,0 +1,38 @@
+#Laboratorio 9
+#Realizado por: Alejandro Gómez y Marco Jurado
+#Assembler
+#Fecha creación: 17/05/2021
+#Ultima modificación: 17/05/2021
+
+
+
+#Se definen los imports
+
+import RPi.GPIO as GPIO
+import time 
+
+#Se define variable para menú:
+
+IngresoUsuario = " "
+OpcionMenu = True
+
+#Se define función del menú:
+
+def MenuLab9():
+    print("Bienvenido al Laboratorio No.9")
+    print("Para acceder a las opciones utilice los siguientes comandos: ")
+    print(" Y: para iniciar corrimiento de bits inactivos")
+    print(" Q: para detener el corrimiento de bits inactivos ")
+
+# Se define ciclo del menu
+while OpcionMenu:
+    MenuLab9()
+    IngresoUsuario = input("Por favor, ingrese su accion: ")
+    if (IngresoUsuario == "Y"):
+        print("El programa iniciara a funncionar...")
+        print("El corrimiento de bits ha iniciado: ")
+
+    if(IngresoUsuario == "Q"):
+        print("Gracias por utilizar el programa.")
+        print("El corrimiento de bits inactivos se ha detenido")
+        OpcionMenu = False

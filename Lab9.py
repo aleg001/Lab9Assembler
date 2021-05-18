6226960dcfc59bb151aa47aa38a029542c906e09
Ale Gómez <60187741+aleg001@users.noreply.github.com>
2021-05-17 20:42:25 -0600

    Versión final

--- Lab9.py
+++ Lab9.py
@@ -12,39 +12,83 @@ import RPi.GPIO as GPIO
 import time 
 
 #Se define los modos para la GPIO
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(PONER NUMERO, GPIO.out)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+#Se configura para cada salida de led
+GPIO.setup(3, GPIO.out)
+GPIO.setup(5, GPIO.out)
+GPIO.setup(7, GPIO.out)
+GPIO.setup(11, GPIO.out)
+GPIO.setup(13, GPIO.out)
 
 #Se define variable para menú:
-
 IngresoUsuario = " "
 OpcionMenu = True
 
 #Se define función del menú:
 
 def MenuLab9():
-    print("Bienvenido al Laboratorio No.9")
-    print("Para acceder a las opciones utilice los siguientes comandos: ")
-    print(" Y: para iniciar corrimiento de bits inactivos")
-    print(" Q: para detener el corrimiento de bits inactivos ")
+    print("\033[4m >>Bienvenido al Laboratorio No.9 \033[0;0m ")
+    print(" Para acceder a las opciones utilice los siguientes comandos: ")
+    print("   Y: para iniciar corrimiento de bits inactivos")
+    print("   Q: para detener el corrimiento de bits inactivos ")
 
 # Se define ciclo del menu
 while OpcionMenu:
 
     MenuLab9()
     IngresoUsuario = input("Por favor, ingrese su accion: ")
-
+     
     #Se define condiciones de inicio:
-    if (IngresoUsuario == "Y"):
-        print("El programa iniciara a funncionar...")
-        print("El corrimiento de bits ha iniciado: ")
+    if (IngresoUsuario == "Y" or IngresoUsuario == "y"):
+        print(" >>El programa iniciara a funcionar...")
+        print(" *Beeep* *Boop* *ZzzZzz* \n >>La secuencia ha iniciado: ")
+
+        #Todas encendidas - estado inicial
+        GPIO.output(3, True)
+        GPIO.output(5, True)
+        GPIO.output(7, True)
+        GPIO.output(11, True)
+        GPIO.output(13, True)
+        print("*Buuuuzzzzz*")
+        time.sleep(1)
+
+        #Estado 1
+        GPIO.output(13, False)
+        print("*Tzzzsss*")
+        time.sleep(1)
+
+
+        #Estado 2
+        GPIO.output(11, False)
+        print("*Zaaaazzzz*")
+        time.sleep(1)
+
+        #Estado 3
+        GPIO.output(7, False)
+        print("*beeeeeep*")
+        time.sleep(1)
+
+        #Estado 4
+        GPIO.output(5, False)
+        print("*Pin Pan*")
+        time.sleep(1)
+
+        #Estado 5 - FINAL
+        GPIO.output(3, False)
+        #TERMINA SECUENCIA
+
 
     #Se definen condiciones de salida:
-    if(IngresoUsuario == "Q"):
-        print("Gracias por utilizar el programa.")
-        print("El corrimiento de bits inactivos se ha detenido")
+    if(IngresoUsuario == "Q" or IngresoUsuario == "q"):
+        print(" >>Gracias por utilizar el programa.")
+        print(" >>El corrimiento de bits inactivos se ha detenido")
+        #Los puertos GPIO se limpian
+        GPIO.cleanup()
         OpcionMenu = False
         
     #Se definen condiciones en caso se ingrese algo erroneo
     else:
-        print("Cuidado... Ese comando no existe...")
+        print(" >>Cuidado... Ese comando no existe...")
+
